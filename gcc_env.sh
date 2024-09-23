e82efcdb51ad696a0985acb7ad04de549b4b88db
young <67495416+quarter-back@users.noreply.github.com>
2024-09-23 10:23:56 +0800

    Update gcc_env.sh

--- gcc_env.sh
+++ gcc_env.sh
@@ -1,4 +1,5 @@
-yum install centos-release-scl
-yum install devtoolset-8
+sudo yum install centos-release-scl
+sudo yum install scl-utils
+sudo yum install devtoolset-8
 # scl enable devtoolset-8 bash
 source /opt/rh/devtoolset-8/enable
